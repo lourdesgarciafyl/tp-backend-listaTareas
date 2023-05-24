6ecddb19f1e7fa1b0b9dd69fa514c527e0f1072f
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-24 17:30:19 -0300

    controlamos el input con el control tarea

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,12 +1,21 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTareas = () => {
+    const [tarea, setTarea] = useState(``);
+    const [tareas, setTareas] = useState([]);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <section>
-        <Form>
+        <Form onSubmit={handleSubmit}> 
+                {/* funcion sin parentesis */}
             <Form.Group  className="mb-3 d-flex" controlId="tarea">
-                <Form.Control type="text" placeholder="Ingrese una tarea" />
+                <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=> setTarea(e.target.value)} value={tarea}/>
                 <Button variant="primary" type="submit" className="ms-1">Enviar
                 </Button>
             </Form.Group>
