7dc5f6fa0f32355dcf2a4cbe86295a6237df9998
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-24 17:45:32 -0300

    creo funcion para agregar al array

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -3,17 +3,19 @@ import ListaTareas from "./ListaTareas";
 import { useState } from "react";
 
 const FormularioTareas = () => {
-    const [tarea, setTarea] = useState(``);
+    const [tarea, setTarea] = useState("");
     const [tareas, setTareas] = useState([]);
     
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        setTareas([...tareas, tarea]);
+        setTarea(``)
     }
 
     return (
         <section>
         <Form onSubmit={handleSubmit}> 
-                {/* funcion sin parentesis */}
+                {/* funcion sin parentesis, en caso de que si invocar la funcion como anonima y ahi poner los parametros () => handleSubmit(parametro) */}
             <Form.Group  className="mb-3 d-flex" controlId="tarea">
                 <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=> setTarea(e.target.value)} value={tarea}/>
                 <Button variant="primary" type="submit" className="ms-1">Enviar
