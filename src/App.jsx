9b1d7b6e4c5f9a891486fe8231bbe777dd5600b7
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-24 09:59:09 -0300

    se agregan los estilos basicos en app.css, el titulo y el footer en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
 
   return (
     <>
-
+    <Container className='my-4 containerPrincipal'>
+      <h1 className='display-2 text-center'>Lista de tareas</h1>
+      <hr />
+    </Container>
+    <footer className='bg-dark text-light text-center py-3'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
