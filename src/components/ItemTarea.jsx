cb399d34ac7043d073c9492cbc76857e587dd08e
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-29 16:59:50 -0300

    hotfix en item tarea, para que se separan unas de otras

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -3,9 +3,9 @@ import { Button, ListGroup } from "react-bootstrap";
 const ItemTarea = ({ tarea, funcionBorrarParaItem }) => {
   return (
     <div>
-      <ListGroup.Item className="d-flex justify-content-between">
+      <ListGroup.Item className="d-flex justify-content-between  mb-2 pt-3">
         {tarea}
-        <Button variant="danger" onClick={() => funcionBorrarParaItem(tarea)}>Borrar</Button>
+        <Button className="pt-1" variant="danger" onClick={() => funcionBorrarParaItem(tarea)}>Borrar</Button>
       </ListGroup.Item>
     </div>
   );
