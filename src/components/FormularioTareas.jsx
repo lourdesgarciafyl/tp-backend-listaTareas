ad73a83007da1480663f905914109782114734f3
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-29 17:24:31 -0300

    guardamos en local storage la lista de tareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,10 +1,20 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FormularioTareas = () => {
+    // comenzar por guardar en el local storage
+    let tareasLocalStorage = JSON.parse(localStorage.getItem("listaTareas")) || [];
     const [tarea, setTarea] = useState("");
-    const [tareas, setTareas] = useState([]);
+    const [tareas, setTareas] = useState(tareasLocalStorage);
+    
+    useEffect(() => {
+        localStorage.setItem("listaTareas", JSON.stringify(tareas))
+        // si uso solo la funcion, me interesa el montaje y actualizaicon de TODOS los state
+        //si quiero decir que siempre funcione en montaje y en la actualizacion de ALGUNOS states, debo agregar al final de la funcion ", y el otro elemento"
+    }, [tareas])
+    // aqui arriba le digo: solo me interesa los cambios que hay en tareas, no en el array tarea (auqi puedo incluir tantos state como quiera)
+    // otra version del useEffect: despues de la funcion, incluir ", []" indicando que me importa solo el MONTAJE, ninguna actualizacion de ningun state
     
     const handleSubmit = (e) => {
         e.preventDefault();
