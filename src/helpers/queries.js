c60d2d99261d52ca68904c157a22d235458e2543
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-06-23 08:58:28 -0300

    se crean las funciones para consultar lista tareas, consultar una tarea, crear una tarea y borrar una tarea

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,1 +1,51 @@
 const URLTareas = import.meta.env.VITE_API_TAREAS
+
+export const consultarListaTareas = async () =>{
+    try{
+        const respuesta = await fetch(URLTareas)
+        const listaTareas = await respuesta.json()
+        return listaTareas
+    }catch(error){
+        console.log(error)
+        return null
+    }
+}
+
+export const consultarTarea = async (id) =>{
+    try{
+        const respuesta = await fetch(URLTareas`/${id}`);
+        const tarea = await respuesta.json()
+        return tarea
+    }catch(error){
+        console.log(error)
+        return null 
+    }
+}
+
+export const consultarCrearTarea = async (tarea) => {
+    try{
+        const respuesta = await fetch(URLTareas, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(tarea)
+        })
+        return respuesta
+    }catch(error){
+        console.log(error)
+        return null 
+    }
+}
+
+export const consultarBorrarTarea = async (id) =>{
+    try{
+        const respuesta = await fetch(URLTareas`/${id}`, {
+            method: "DELETE"
+        })
+        return respuesta
+    }catch(error){
+        console.log(error)
+        return null
+    }
+}
